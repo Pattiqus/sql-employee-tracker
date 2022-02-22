90e82c611f110e336244b197cbfcb33564a6f03c
unknown <patticus.tv@gmail.com>
2022-02-22 18:34:37 +1100

    begun index file

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql2");
+var consoleTable = require("console.table");
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// # Import: Node Modules
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const consoleTable = require("console.table");
+
+// # Secure: Sensitive data
+require("dotenv").config();
+
+// # Connect: To SQL database
